1d91975dd7d28e5cde6b15cf4cc579ed243a469f
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-20 08:47:44 -0700

    commit tree d60cd617a77e5dcc3f55f1be42df158e69ffa65e

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return ('This one is on me!');
+  } else if (ride > 2000 && ride <= 2500){
+    return ('I will gladly take your thirty bucks.');
+  } else if (ride > 2500){
+    return('No can do.')
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
 function switchOnCharmFromTip(){
